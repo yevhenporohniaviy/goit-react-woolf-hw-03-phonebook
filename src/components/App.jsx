2851cdbaed6dd83c2a1c1a40c8d6afd18101087b
yevhenporohniaviy <porohniaviy.yevheni@gmail.com>
2024-04-22 11:50:21 +0300

    feature:hw-03

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,19 @@ export class App extends Component {
     ],
     filter: '',
   };
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts.length !== prevState.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   handleAddContect = data => {
     const isInContacts = this.state.contacts.some(
       ({ name }) => name.toLowerCase() === data.name.toLowerCase()
